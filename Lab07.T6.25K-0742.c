2f6d12085688c60c40e518b133d204599ca87e79
Mahnoor Farrukh <k250742@nu.edu.pk>
2025-10-21 15:41:31 +0500

    Add files via upload

---
+++ Lab07.T6.25K-0742.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char str[30];
+    int vowel=0,consonant=0; 
+    printf("Enter your word");
+    scanf("%s",str);
+    int i=0;
+    while(str[i]!='\0'){
+        if(str[i]=='a'|| str[i]=='A'||str[i]=='e'|| str[i]=='E'||str[i]=='i'|| str[i]=='I'||str[i]=='o'|| str[i]=='O'||str[i]=='u'|| str[i]=='U'){
+            vowel++;
+        }
+        else{
+            consonant++;
+        }
+        i++;
+    }
+printf("The number of vowels in your word are: %d\n",vowel);
+printf("The number of consonants in your word are: %d\n",consonant);
+return 0; 
+}
